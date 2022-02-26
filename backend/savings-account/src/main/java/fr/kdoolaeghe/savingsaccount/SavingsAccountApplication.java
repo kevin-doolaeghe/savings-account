edfd292e5302dea23e426fccdf6e3227a0f900ca
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2022-02-26 22:11:08 +0100

    starter base

--- backend/savings-account/src/main/java/fr/kdoolaeghe/savingsaccount/SavingsAccountApplication.java
+++ backend/savings-account/src/main/java/fr/kdoolaeghe/savingsaccount/SavingsAccountApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SavingsaccountApplication {
+public class SavingsAccountApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SavingsaccountApplication.class, args);
+		SpringApplication.run(SavingsAccountApplication.class, args);
 	}
 
 }
--- backend/savings-account/src/test/java/fr/kdoolaeghe/savingsaccount/SavingsAccountApplicationTests.java
+++ backend/savings-account/src/test/java/fr/kdoolaeghe/savingsaccount/SavingsAccountApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SavingsaccountApplicationTests {
+class SavingsAccountApplicationTests {
 
 	@Test
 	void contextLoads() {
